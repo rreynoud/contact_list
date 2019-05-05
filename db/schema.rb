d7f3b7692db5645d9b251c7a07e8ed48595930b5
Rafael Reynoud <rreynoud@gmail.com>
2019-05-05 22:21:30 +0000

    com scaffold criado crud de contato

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190505213052) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
